e4718bdf584f21739f8497b65a8cca3b12172ae3
Vitaliy Popov <ultraon@mail.ru>
2016-11-19 16:44:56 +0200

    TDD: add new test

---
+++ src/test/java/com/ultraon/testing/ZhabaTest.java
@@ -0,0 +1,13 @@
+package com.ultraon.testing;
+
+import org.junit.Test;
+
+/**
+ * Created by vitaliypopov on 19/11/16.
+ */
+public class ZhabaTest {
+    @Test
+    public void test() {
+        
+    }
+}
