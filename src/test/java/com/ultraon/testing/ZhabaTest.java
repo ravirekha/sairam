3bc65bc2b0fc2de154c4a477363862d8e4019372
Vitaliy Popov <ultraon@mail.ru>
2016-11-19 17:38:29 +0200

    TDD: Added testSeveralWords()

--- src/test/java/com/ultraon/testing/ZhabaTest.java
+++ src/test/java/com/ultraon/testing/ZhabaTest.java
@@ -11,42 +11,49 @@ public class ZhabaTest {
     @Test
     public void testEmptyString() {
         final Reversal reversal = new Reversal();
-        Truth.assertThat(reversal.reverse("")).isNotNull();
+        Truth.assertThat(reversal.reverseWord("")).isNotNull();
     }
 
     @Test
     public void testOneLetter() {
         final Reversal reversal = new Reversal();
-        Truth.assertThat(reversal.reverse("a")).isEqualTo("a");
+        Truth.assertThat(reversal.reverseWord("a")).isEqualTo("a");
     }
 
     @Test
     public void testTwoLetters() {
         final Reversal reversal = new Reversal();
-        Truth.assertThat(reversal.reverse("ab")).isEqualTo("ba");
+        Truth.assertThat(reversal.reverseWord("ab")).isEqualTo("ba");
     }
 
     @Test
     public void testOneNonLetter() {
         final Reversal reversal = new Reversal();
-        Truth.assertThat(reversal.reverse("1")).isEqualTo("1");
+        Truth.assertThat(reversal.reverseWord("1")).isEqualTo("1");
     }
 
     @Test
     public void testOneLetterAndOneNonLetter() {
         final Reversal reversal = new Reversal();
-        Truth.assertThat(reversal.reverse("1a")).isEqualTo("1a");
+        Truth.assertThat(reversal.reverseWord("1a")).isEqualTo("1a");
     }
 
     @Test
     public void testSeveralLetterAndNonLetter() {
         final Reversal reversal = new Reversal();
-        Truth.assertThat(reversal.reverse("abaj1")).isEqualTo("jaba1");
+        Truth.assertThat(reversal.reverseWord("abaj1")).isEqualTo("jaba1");
     }
 
     @Test
     public void testSeveralLetterAndSeveralNonLetter() {
         final Reversal reversal = new Reversal();
-        Truth.assertThat(reversal.reverse("1abaj1")).isEqualTo("1jaba1");
+        Truth.assertThat(reversal.reverseWord("1abaj1")).isEqualTo("1jaba1");
     }
+
+    @Test
+    public void testSeveralWords() {
+        final Reversal reversal = new Reversal();
+        
+    }
+
 }
